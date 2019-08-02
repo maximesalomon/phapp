f3c83222b5eb3816eee757a9c3d579b30b331e62
Maxime Salomon <maxime@croissant.io>
2019-08-02 19:05:29 +0200

    from class ro hooks

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const PH_API = "https://api.producthunt.com/v1/"
 const PH_TOKEN =`${process.env.REACT_APP_PH_TOKEN}`
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      posts: []
-    };
-  }
+function App() {
+  const [posts, setPosts] = useState([]);
 
-  componentDidMount() {
+  useEffect(() => {
     let config = {
       headers: {
         'Authorization': "Bearer " + PH_TOKEN
@@ -21,29 +16,26 @@ class App extends Component {
     axios
       .get(PH_API + "/posts", config)
       .then(res => {
-        this.setState({posts: res.data.posts})
+        setPosts(res.data.posts)
       })
-  }
+  }, [])
 
-  render() {
-    console.log();
-    return (
-      <div className="container">
-        <h1 className="text-red-700">ProductHunt + Payfit</h1>
-        <div>
-          {
-            this.state.posts.map((post, idx) => {
-                return (
-                <div key={idx}>
-                  <p>{post.votes_count} | {post.name} - {post.tagline}</p>
-                </div>
-                )
-            })
-          }
-        </div>
+  return (
+    <div className="container">
+      <h1 className="text-red-700">ProductHunt + Payfit</h1>
+      <div>
+        {
+          posts.map((post, idx) => {
+              return (
+              <div key={idx}>
+                <p>{post.votes_count} | {post.name} - {post.tagline}</p>
+              </div>
+              )
+          })
+        }
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
