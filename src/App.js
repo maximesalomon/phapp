5fa56d5fef9945395ab95aaf31978321d6559739
Maxime Salomon <maxime@croissant.io>
2019-08-03 16:35:22 +0200

    edit login redirect url

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ function App() {
           </a>
         </div>
         <div>
-          <a href="https://api.producthunt.com/v2/oauth/authorize?client_id=e14e77d9332895fbb8136b11380eb397f4d7275a39c5e7b3b9b06c2a11eedccc&redirect_uri=https://croissant.io&response_type=code&scope=public+private" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-orange-600 hover:bg-white mt-4 lg:mt-0">Login</a>
+          <a href="https://api.producthunt.com/v2/oauth/authorize?client_id=e14e77d9332895fbb8136b11380eb397f4d7275a39c5e7b3b9b06c2a11eedccc&redirect_uri=https://phpayfit.netlify.com&response_type=code&scope=public+private" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-orange-600 hover:bg-white mt-4 lg:mt-0">Login</a>
         </div>
       </div>
     </nav>
