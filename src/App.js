5af755279a385eb142fcff4995f28314591f4262
Maxime Salomon <maxime@croissant.io>
2019-08-03 16:32:29 +0200

    Add navbar

--- src/App.js
+++ src/App.js
@@ -49,20 +49,42 @@ function App() {
     }, [fetchedPosts]))
 
   return (
-    <div className="container">
-      <h1 className="text-orange-600 mt-4 mb-4 px-4 text-3xl font-medium">ProductHunt + Payfit</h1>
+    <div>
+      <nav class="flex items-center justify-between flex-wrap bg-orange-600 p-6">
+      <div class="flex items-center flex-shrink-0 text-white mr-6">
+        <span class="font-semibold text-xl tracking-tight">ProductHunt + Payfit</span>
+      </div>
+      <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+        <div class="text-sm lg:flex-grow">
+          <a href="https://api.producthunt.com/v2/docs" class="block mt-4 lg:inline-block lg:mt-0 text-white hover:font-medium mr-4">
+            PH GraphQL API Docs
+          </a>
+          <a href="https://twitter.com/maximesalomon" class="block mt-4 lg:inline-block lg:mt-0 text-white hover:font-medium mr-4">
+            Build by maximesalomon
+          </a>
+        </div>
+        <div>
+          <a href="https://api.producthunt.com/v2/oauth/authorize?client_id=e14e77d9332895fbb8136b11380eb397f4d7275a39c5e7b3b9b06c2a11eedccc&redirect_uri=https://croissant.io&response_type=code&scope=public+private" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-orange-600 hover:bg-white mt-4 lg:mt-0">Login</a>
+        </div>
+      </div>
+    </nav>
+      <div className="mt-4 ml-8">
         {
-          posts.map((post, idx) => {
+          posts.length === 0
+          ? <p>Loading...</p>
+          : posts.map((post, idx) => {
               return (
-              <div className="rounded py-4 mb-3 ml-4 flex border bg-white border-orange-600 " key={idx}>
+                
+              <div key={idx} className="relative rounded py-4 mb-3 ml-4 w-8/12 flex border bg-white border-orange-600">
                 <p className="text-orange-600 font-medium ml-4 w-8">{post.node.votesCount}</p>
-                <p className="ml-4 w-64 font-medium">{post.node.name}</p>
+                <p className="ml-4 w-1/4 font-medium">{post.node.name}</p>
                 <p className="w-auto inline-block align-middle text-sm">{post.node.tagline}</p>
-                <p className="ml-4 text-orange-600 align-middle text-sm">Learn more</p>
+                <div className="absolute right-0 mr-8"><p className="text-orange-600 align-middle text-sm">Learn more</p></div>
               </div>
               )
           })
         }
+      </div>
     </div>
   );
 }
