6dcf3ce642f333605a0f5f341e3d50c84fb14a3d
Maxime Salomon <maxime@croissant.io>
2019-08-31 23:25:32 +0200

    Navbar padding update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,10 +7,10 @@ const Navbar = ({ loggedIn, setLoggedIn, name }) => {
   };
 
   return (
-    <nav className="flex items-center justify-between flex-wrap bg-phorange p-6">
+    <nav className="flex items-center justify-between flex-wrap bg-phorange pl-6 pr-6 p-3">
       <div className="flex items-center flex-shrink-0 text-white mr-6">
         <span className="font-semibold text-xl tracking-tight">
-          PHapp
+          PH MaxReader
         </span>
       </div>
       <div>
