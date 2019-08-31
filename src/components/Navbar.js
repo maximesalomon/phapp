92bb231d4d66170556cd4002a2453b780082fa48
Maxime Salomon <maxime@croissant.io>
2019-08-31 23:22:06 +0200

    remove navbar links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,24 +10,9 @@ const Navbar = ({ loggedIn, setLoggedIn, name }) => {
     <nav className="flex items-center justify-between flex-wrap bg-phorange p-6">
       <div className="flex items-center flex-shrink-0 text-white mr-6">
         <span className="font-semibold text-xl tracking-tight">
-          ProductHunt + Payfit
+          PHapp
         </span>
       </div>
-      <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-        <div className="text-sm lg:flex-grow">
-          <a
-            href="https://api.producthunt.com/v2/docs"
-            className="block mt-4 lg:inline-block lg:mt-0 text-white hover:font-medium mr-4"
-          >
-            PH GraphQL API Docs
-          </a>
-          <a
-            href="https://twitter.com/maximesalomon"
-            className="block mt-4 lg:inline-block lg:mt-0 text-white hover:font-medium mr-4"
-          >
-            Built by maximesalomon
-          </a>
-        </div>
         {loggedIn === false ? (
           <div>
             <a
