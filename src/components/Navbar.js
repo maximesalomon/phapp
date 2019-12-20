db4310ddf8254a15510b3a122f76452d729db34f
Maxime Salomon <maxime@croissant.io>
2019-12-20 13:00:14 +0100

    prettier

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,9 +9,7 @@ const Navbar = ({ loggedIn, setLoggedIn, name }) => {
   return (
     <nav className="flex items-center justify-between flex-wrap bg-phorange pl-6 pr-6 p-3">
       <div className="flex items-center flex-shrink-0 text-white mr-6">
-        <span className="font-semibold text-xl tracking-tight">
-          MaxPH
-        </span>
+        <span className="font-semibold text-xl tracking-tight">MaxPH</span>
       </div>
       <div>
         {loggedIn === false ? (
